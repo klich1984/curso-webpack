d60ce3f72ed1b92a95807d345609330564b63b9f
Carlos Usuga Martinez <klich84@gmail.com>
2021-11-12 13:27:09 -0500

    add loader css and preprocess stylus of class number 8

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path'),
-	HtmlWebpackPlugin = require('html-webpack-plugin')
+	HtmlWebpackPlugin = require('html-webpack-plugin'),
+	MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
 	entry: './src/index.js' /* elemento de entrada */,
@@ -19,13 +20,18 @@ module.exports = {
 					loader: 'babel-loader',
 				},
 			},
+			{
+				test: /\.css|.styl$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
+			},
 		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			inject: true,
 			template: './public/index.html',
-			filename: './index.html'
-		})
-	]
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin(),
+	],
 }
